561db27cb6b875ed348a395beb92c2a57cb2bc8b
Matthew Grill <matt@fourkitchens.com>
2014-11-11 16:21:52 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express         = require('express'),
+    app             = express(),
+    subdomain       = require('wildcard-subdomains'),
+    client          = require('google-images');
+
+app
+  .set('views', './views')
+  .set('view engine', 'jade')
+  .use(subdomain({
+    domain : 'avocado.republican',
+    www : true
+  }))
+  .get('/', function (req, res) {
+    res.redirect(302, 'http://taco.avocado.republican');
+  })
+  .get('/sub/www', function (req, res) {
+    res.redirect(302, 'http://taco.avocado.republican');
+  })
+  .get('/sub/:subdomain', function (req, res) {
+    client.search('avocado' + req.params.subdomain, function (err, images) {
+      res.render('index', { term: req.params.subdomain, image: images[Math.floor(Math.random() * images.length)].url });
+    });
+  })
+  .listen(3000);
